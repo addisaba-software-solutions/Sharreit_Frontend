692cc88078583349a72ab639932c12d8dbe9dd60
Yohannes Berhanu <Johnlights51@gmail.com>
2020-05-06 11:51:04 +0300

    Wednesday Morning

--- src/Components/Registration/index.jsx
+++ src/Components/Registration/index.jsx
@@ -23,7 +23,7 @@ import Logo from "../../Assets/Group.svg";
 // import Stepper from '@material-ui/core/Stepper';
 // import Step from '@material-ui/core/Step';
 // import StepLabel from '@material-ui/core/StepLabel';
-import clsx from 'clsx';
+import clsx from "clsx";
 import PersonalInformation from "./components/personalInformation";
 import ImageUpload from "./components/imageUpload";
 
@@ -31,42 +31,43 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
-      iWork PLC
+      SharreIt
       {new Date().getFullYear()}
       {"."}
     </Typography>
   );
 }
-
 export default function Registration() {
   const classes = useStyles();
-
   return (
-    <Grid container component="main" className={classes.root}>
-      <CssBaseline />
-      <Grid
-        item
-        xs={12}
-        sm={8}
-        md={5}
-        component={Paper}
-        elevation={6}
-        square
-        className={classes.paperContainer}
-      >
-        <Box className={classes.paper}>
-          <img src={Logo} className={classes.Logo} alt="" />
-          <Avatar className={classes.avatar}>
-            <LockOutlined />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign Up
-          </Typography>
+    <>
+      <Grid container component="main" className={classes.root}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={12}
+          sm={8}
+          md={5}
+          component={Paper}
+          elevation={6}
+          square
+          className={classes.paperContainer}
+        >
+          <Box className={classes.paper}>
+            <img src={Logo} className={classes.Logo} alt="" />
+            <Avatar className={classes.avatar}>
+              <LockOutlined />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign Up
+            </Typography>
 
-    <PersonalInformation />
-        </Box>
+            <PersonalInformation />
+          </Box>
+          <Copyright />
+        </Grid>
+        <Grid item xs={false} sm={4} md={7} className={classes.image} />
       </Grid>
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
-    </Grid>
+    </>
   );
 }
